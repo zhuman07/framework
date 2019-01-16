e22215d6f880b7d095ef66031645620552a064d8
zhuman <zhuman1995@gmail.com>
2019-01-16 18:40:34 +0600

    test branch

--- index.php
+++ index.php
@@ -24,3 +24,35 @@ spl_autoload_register( 'replaceUnderscores') ;
 spl_autoload_register('namespaceAutoLoad');
 
 Router::getInstance()->execute();
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=test user=test password=test")
+or die('Could not connect: ' . pg_last_error());
+
+$query = 'SELECT * FROM posts';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+pg_close($dbconn);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    <?php while ($row = pg_fetch_object($result)): ?>
+    <li>
+        Title: <?php echo $row->title ?><br>
+        Description: <?php echo $row->description ?><br>
+        Date: <?php echo $row->date ?><br>
+        <hr>
+    </li>
+    <?php endwhile; ?>
+</ul>
+</body>
+</html>
